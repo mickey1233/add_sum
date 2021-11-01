eb7793538bf43ab98bb47c96f78d0127c70b8331
mickey1233 <42233582+mickey1233@users.noreply.github.com>
2021-11-01 19:16:28 +0800

    Add files via upload

---
+++ add_sum.py
@@ -0,0 +1,9 @@
+def addstrings(num1, num2):
+    sum = int(num1) + int(num2)
+    return str(sum)
+
+
+if __name__ == "__main__":
+    print(addstrings("11", "123"))
+    print(addstrings("456", "77"))
+    print(addstrings("0", "0"))
